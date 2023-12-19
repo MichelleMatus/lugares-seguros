91d9e809e15c250928fea127cd6a8ee713eef3a1
MichelleMatus <jaydenmatus@gmail.com>
2023-12-19 00:59:02 -0600

    cambios en home y data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import { Button } from "antd"
 import { AppLayout } from "./components/Layout";
-import { Home } from "./views/Home";
+import { Home } from "./views/HomePage";
 import './App.css'
 
 function App() {
 
   return (
-    <Routes styles={{width: "100vw"}}>
+    <Routes styles={{width: "100vw"}}> 
       <Route element={<AppLayout />}>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<>Home</>} />
         <Route path="/place/details" element={<>Place Details</>} />
         <Route path="/place/create" element={<>Create Place</>} />
         <Route path="/register" element={<>Register</>} />
         <Route path="/login" element={<>Login</>} />
         <Route path="/profile" element={<>Profile</>} />
       </Route>
-    </Routes>
+      </Routes>
   )
 }
 
